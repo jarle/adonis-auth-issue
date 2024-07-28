79b89840e4dfff53d62b6cee04f8a46fb4f6e8cb
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-07-28 19:46:24 +0200

    add code for bug

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,11 @@
-import { DateTime } from 'luxon'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, column, hasOne } from '@adonisjs/lucid/orm'
+import type { HasOne } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import Post from './post.js'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -10,6 +13,8 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+
   @column({ isPrimary: true })
   declare id: number
 
@@ -27,4 +32,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @hasOne(() => Post)
+  declare post: HasOne<typeof Post>
 }
--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,11 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/foo', async ({ auth }) => {
+  // here the signature of the load method is of type _any_
+  await auth.user?.load('post')
+  // this gives type error (Argument of type '"post"' is not assignable to parameter of type 'undefined'.ts(2345))
+  await auth.user?.related('post').query().first()
+  return null
+})
